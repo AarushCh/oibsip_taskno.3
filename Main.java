e1ec67941219673ad11f0a850925c68aa645715f
Ch Aarush Udbhav <116322111+AarushCh@users.noreply.github.com>
2024-12-01 22:07:42 +0530

    ATM Interface
    The ATM Interface is a Java-based console application that simulates a basic ATM system. It allows users to securely log in, view their balance, make withdrawals, deposits, transfers, and view transaction history. The system provides a simple, menu-driven interface where users can perform various banking operations. It demonstrates the use of object-oriented programming concepts such as classes, methods, and data handling, offering a practical example of how an ATM system operates in real life.

---
+++ Main.java
@@ -0,0 +1,32 @@
+package ATM_Interface_AUC;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank();
+        
+        bank.addUser(new User("Freshu", "8484", 3500));
+        bank.addUser(new User("AUC", "6801", 10000));
+        bank.addUser(new User("Dodo", "4200", 5000));
+        bank.addUser(new User("Mino", "7878", 1500));
+
+        System.out.println("\nWelcome to the ATM!");
+        System.out.print("\nEnter User ID: ");
+        String userId = scanner.nextLine();
+        System.out.print("Enter PIN: ");
+        String pin = scanner.nextLine();
+        System.out.print("\n");
+
+        User currentUser = bank.authenticateUser(userId, pin);
+        if (currentUser != null) {
+            ATM atm = new ATM(currentUser, scanner);
+            atm.start();
+        } else {
+            System.out.println("Invalid credentials. Exiting.");
+        }
+
+        scanner.close();
+    }
+}
